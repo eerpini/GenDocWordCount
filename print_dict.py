0b495d0a0cc86a2fdb67e2561be969f751c2656e
satish <eerpini@gmail.com>
2010-06-27 01:26:36 +0530

    add printing to a file support to the print_dict module

--- print_dict.py
+++ print_dict.py
@@ -1,12 +1,15 @@
-def print_d(dict, asarray=0):
+import sys 
+
+def print_d(dict, asarray=0, file_handle=sys.stdout):
     """print the key value pairs in the dictionary in a neat way
     pass the second argument as 1 in case you want the values in the 
-dictionary to be printed in a array like vector fashion."""
+dictionary to be printed in a array like vector fashion. And pass the file
+handle of the file if you want the function to print to a file"""
     if asarray ==1 :
-        print "[ ",
+        print >>file_handle, "[ ",
         for value in sorted(dict.keys()):
-            print "%d " % dict[value],
-        print " ]"
+            print >>file_handle, "%d " % dict[value],
+        print >>file_handle, " ]"
     else:
         for value in sorted(dict.keys()):
-            print " %10s   :   %3d" % (value, dict[value])
+            print >>file_handle, " %10s   :   %3d" % (value, dict[value])
