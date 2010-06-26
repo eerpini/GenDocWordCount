3c5f7d57f97e3a2c8139d3a28337df48cdd93435
satish <eerpini@gmail.com>
2010-06-27 01:27:54 +0530

    small change in print_dict's __doc__

--- print_dict.py
+++ print_dict.py
@@ -4,7 +4,7 @@ def print_d(dict, asarray=0, file_handle=sys.stdout):
     """print the key value pairs in the dictionary in a neat way
     pass the second argument as 1 in case you want the values in the 
 dictionary to be printed in a array like vector fashion. And pass the file
-handle of the file if you want the function to print to a file"""
+handle of the file if you want the function to print to a file, prints to stdout by default"""
     if asarray ==1 :
         print >>file_handle, "[ ",
         for value in sorted(dict.keys()):
